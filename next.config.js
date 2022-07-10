762e8351bfbcddbbb5980d657b243f4e33784285
smakosh <ismai23l@hotmail.com>
2022-07-10 03:04:20 +0200

    chore: adjust next config and upgrade tailwindcss related tools

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-	reactStrictMode: true,
-	images: {
-		domains: ["images.unsplash.com"],
-	},
+  reactStrictMode: true,
+  images: {
+    domains: ['images.unsplash.com'],
+  },
+  ignoreBuildErrors: true,
 };
