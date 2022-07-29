fd61a9e5dcfcdbbba0975c1b4bd6177c28d2b027
smakosh <ismai23l@hotmail.com>
2022-07-29 22:49:39 +0200

    feat: use Wagmi read/write contract hooks and show etherscan and opensea links

--- src/features/unsplash/modules/MetadataForm/index.tsx
+++ src/features/unsplash/modules/MetadataForm/index.tsx
@@ -1,16 +1,70 @@
+import { useEffect, useState } from 'react';
 import { ErrorMessage, Field, Form, Formik } from 'formik';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import * as Yup from 'yup';
-import { useSigner } from 'wagmi';
-import { createNFT, MetadataNFT } from 'features/unsplash/actions';
-import { AppDispatch, RootState } from 'utils/store';
+import axios from 'axios';
+import { create as ipfsHttpClient } from 'ipfs-http-client';
+import { ethers } from 'ethers';
+import {
+  useSigner,
+  usePrepareContractWrite,
+  useContractWrite,
+  useWaitForTransaction,
+  useNetwork,
+  useContractRead,
+  useAccount,
+} from 'wagmi';
+import { MetadataNFT } from 'features/unsplash/actions';
+import { RootState } from 'utils/store';
+import NFTFactory from '../../../../../artifacts/contracts/NFTFactory.sol/NFTFactory.json';
 
 const MetadataForm = ({ metadata }: { metadata: MetadataNFT }) => {
-  const dispatch = useDispatch<AppDispatch>();
+  const [nftIndex, setNftIndex] = useState(0);
+  const [metadataUrl, setMetadataUrl] = useState<string | undefined>(undefined);
+  const [isUploadingToIPFS, setIsUploadingToIPFS] = useState(false);
   const contract = useSelector((state: RootState) => state.contract);
-  const { data: signer, isLoading } = useSigner();
+  const { address } = useAccount();
+  const { chain } = useNetwork();
+  const { data: signer } = useSigner();
+  const { data: balance } = useContractRead({
+    addressOrName: contract.address as string,
+    contractInterface: NFTFactory.abi,
+    functionName: 'balanceOf',
+    args: [address],
+    watch: true,
+    enabled: !!address,
+  });
+  const { config: contractWriteConfig } = usePrepareContractWrite({
+    addressOrName: contract.address as string,
+    contractInterface: NFTFactory.abi,
+    functionName: 'createToken',
+    args: [metadataUrl],
+    enabled: !!contract.address || !!metadataUrl,
+  });
+  const {
+    data: tokenData,
+    write: createToken,
+    isLoading: isCreatingTokenLoading,
+    isSuccess: isCreateTokenStarted,
+    // error: createTokenError,
+  } = useContractWrite(contractWriteConfig);
+  const {
+    data: txData,
+    isSuccess: txSuccess,
+    // error: txError,
+  } = useWaitForTransaction({
+    hash: tokenData?.hash,
+    enabled: !!tokenData?.hash,
+  });
 
-  const isButtonDisabled = !contract.address && !contract.contract;
+  useEffect(() => {
+    if (balance) {
+      setNftIndex(Number(balance));
+    }
+  }, [balance]);
+
+  const isMinted = txSuccess;
+  const isButtonDisabled = !contract.address;
 
   return (
     <Formik
@@ -30,93 +84,168 @@ const MetadataForm = ({ metadata }: { metadata: MetadataNFT }) => {
         image: Yup.string().required('Image is required'),
         name: Yup.string().required('Name is required'),
       })}
-      onSubmit={async (values, { setSubmitting }) => {
+      onSubmit={async (values) => {
         try {
-          setSubmitting(true);
-          await createNFT({
-            signer,
-            savedContract: contract,
-            values,
-            dispatch,
+          setIsUploadingToIPFS(true);
+          const res: any = await axios.post('/api/ipfs/upload');
+
+          const client = ipfsHttpClient({
+            host: 'ipfs.infura.io',
+            port: 5001,
+            protocol: 'https',
+            headers: {
+              authorization: res.data.auth,
+            },
           });
+
+          // upload image to IPFS
+          const blob = await fetch(values.image).then((r) => r.blob());
+
+          const image = await client.add(blob);
+
+          // upload the rest to IPFS
+          const data = JSON.stringify({
+            attributes: [],
+            ...values,
+            image: `https://ipfs.infura.io/ipfs/${image.path}`,
+          });
+
+          // Upload to IPFS (Look up for alternatives as data is not stored forever)
+          const added = await client.add(data);
+          const metadataUrl = `https://ipfs.infura.io/ipfs/${added.path}`;
+          setMetadataUrl(metadataUrl);
+          setIsUploadingToIPFS(false);
+
+          let currentContract: ethers.Contract;
+
+          // Mint the NFT using the deployed smart contract
+          if (contract.address && createToken) {
+            createToken({
+              recklesslySetUnpreparedArgs: [metadataUrl],
+            });
+          } else if (signer) {
+            currentContract = new ethers.Contract(contract.contract.address, NFTFactory.abi, signer);
+
+            // Token gets created
+            const transaction = await currentContract.createToken(metadataUrl);
+            await transaction.wait();
+          }
         } catch (error) {
           console.log(error);
-        } finally {
-          setSubmitting(false);
         }
       }}
     >
-      {({ isSubmitting }) => (
+      {() => (
         <Form className="bg-white dark:bg-neutral-900 shadow-md rounded px-8 pt-6 pb-8 mb-4 w-96">
-          <div className="mb-4">
-            <label className="block text-gray-700 dark:text-neutral-100 text-sm font-bold mb-2" htmlFor="name">
-              Name
-            </label>
-            <Field
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 dark:text-neutral-200 leading-tight focus:outline-none focus:shadow-outline"
-              id="name"
-              type="text"
-              placeholder="name"
-              name="name"
-            />
-            <ErrorMessage name="name" component="div" className="text-red-500 text-sm" />
-          </div>
-          <div className="mb-4">
-            <label className="block text-gray-700 dark:text-neutral-100 text-sm font-bold mb-2" htmlFor="description">
-              Description
-            </label>
-            <Field
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 dark:text-neutral-200 leading-tight focus:outline-none focus:shadow-outline"
-              id="description"
-              type="text"
-              placeholder="description"
-              name="description"
-              component="textarea"
-              rows="5"
-            />
-            <ErrorMessage name="description" component="div" className="text-red-500 text-sm" />
-          </div>
-          <div className="mb-4">
-            <label className="block text-gray-700 dark:text-neutral-100 text-sm font-bold mb-2" htmlFor="external_url">
-              External URL
-            </label>
-            <Field
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 dark:text-neutral-200 leading-tight focus:outline-none focus:shadow-outline"
-              id="external_url"
-              type="text"
-              placeholder="External URL"
-              name="external_url"
-            />
-            <ErrorMessage name="external_url" component="div" className="text-red-500 text-sm" />
-          </div>
-          <div className="mb-4">
-            <label className="block text-gray-700 dark:text-neutral-100 text-sm font-bold mb-2" htmlFor="name">
-              Image
-            </label>
-            <Field
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 dark:text-neutral-200 leading-tight focus:outline-none focus:shadow-outline"
-              id="image"
-              type="text"
-              placeholder="Image link"
-              name="image"
-            />
-            <ErrorMessage name="image" component="div" className="text-red-500 text-sm" />
-          </div>
-          <div className="flex items-center justify-between">
-            <button
-              className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline ${
-                isButtonDisabled && 'cursor-not-allowed opacity-50'
-              }`}
-              type="submit"
-              disabled={isButtonDisabled}
-            >
-              {isLoading
-                ? 'Getting your signature'
-                : isSubmitting && contract.nftStatus !== null
-                ? contract.nftStatus
-                : 'Mint NFT'}
-            </button>
-          </div>
+          <>
+            <div className="mb-4">
+              <label className="block text-gray-700 dark:text-neutral-100 text-sm font-bold mb-2" htmlFor="name">
+                Name
+              </label>
+              <Field
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 dark:text-neutral-200 leading-tight focus:outline-none focus:shadow-outline"
+                id="name"
+                type="text"
+                placeholder="name"
+                name="name"
+              />
+              <ErrorMessage name="name" component="div" className="text-red-500 text-sm" />
+            </div>
+            <div className="mb-4">
+              <label className="block text-gray-700 dark:text-neutral-100 text-sm font-bold mb-2" htmlFor="description">
+                Description
+              </label>
+              <Field
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 dark:text-neutral-200 leading-tight focus:outline-none focus:shadow-outline"
+                id="description"
+                type="text"
+                placeholder="description"
+                name="description"
+                component="textarea"
+                rows="5"
+              />
+              <ErrorMessage name="description" component="div" className="text-red-500 text-sm" />
+            </div>
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 dark:text-neutral-100 text-sm font-bold mb-2"
+                htmlFor="external_url"
+              >
+                External URL
+              </label>
+              <Field
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 dark:text-neutral-200 leading-tight focus:outline-none focus:shadow-outline"
+                id="external_url"
+                type="text"
+                placeholder="External URL"
+                name="external_url"
+              />
+              <ErrorMessage name="external_url" component="div" className="text-red-500 text-sm" />
+            </div>
+            <div className="mb-4">
+              <label className="block text-gray-700 dark:text-neutral-100 text-sm font-bold mb-2" htmlFor="name">
+                Image
+              </label>
+              <Field
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 dark:text-neutral-200 leading-tight focus:outline-none focus:shadow-outline"
+                id="image"
+                type="text"
+                placeholder="Image link"
+                name="image"
+              />
+              <ErrorMessage name="image" component="div" className="text-red-500 text-sm" />
+            </div>
+            {isMinted ? (
+              <div>
+                <p className="text-green-500">Your NFT has been minted successfully!</p>
+                <p className="mb-2">
+                  View on{' '}
+                  <a
+                    className="underline text-blue-500"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href={
+                      chain?.testnet
+                        ? `https://${chain?.name.toLowerCase()}.etherscan.io/tx/${tokenData?.hash}`
+                        : `https://etherscan.io/tx/${tokenData?.hash}`
+                    }
+                  >
+                    Etherscan
+                  </a>
+                </p>
+                <p>
+                  View on{' '}
+                  <a
+                    className="underline text-blue-500"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href={
+                      chain?.testnet
+                        ? `https://testnets.opensea.io/assets/${chain?.name.toLowerCase()}/${txData?.to}/${nftIndex}`
+                        : `https://opensea.io/assets/${chain?.name.toLowerCase()}/${txData?.to}/${nftIndex}`
+                    }
+                  >
+                    Opensea
+                  </a>
+                </p>
+              </div>
+            ) : (
+              <div className="flex items-center justify-between">
+                <button
+                  className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline ${
+                    isButtonDisabled && 'cursor-not-allowed opacity-50'
+                  }`}
+                  type="submit"
+                  disabled={isButtonDisabled}
+                >
+                  {isUploadingToIPFS && 'Uploading to IPFS...'}
+                  {isCreatingTokenLoading && 'Waiting for approval...'}
+                  {isCreateTokenStarted && 'Minting...'}
+                  {!isCreatingTokenLoading && !isCreateTokenStarted && 'Mint NFT'}
+                </button>
+              </div>
+            )}
+          </>
         </Form>
       )}
     </Formik>
--- src/features/unsplash/modules/SmartContractForm/index.tsx
+++ src/features/unsplash/modules/SmartContractForm/index.tsx
@@ -34,13 +34,13 @@ const SmartContractForm = () => {
       }}
     >
       {({ isSubmitting }) => (
-        <Form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <Form className="bg-whit dark:bg-neutral-900 shadow-md rounded px-8 pt-6 pb-8 mb-4">
           <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">
+            <label className="block text-gray-700 dark:text-neutral-100 text-sm font-bold mb-2" htmlFor="name">
               Name
             </label>
             <Field
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 dark:text-neutral-200 leading-tight focus:outline-none focus:shadow-outline"
               id="name"
               type="text"
               placeholder="name"
@@ -49,11 +49,11 @@ const SmartContractForm = () => {
             <ErrorMessage name="name" component="div" className="text-red-500 text-sm" />
           </div>
           <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="symbol">
+            <label className="block text-gray-700 dark:text-neutral-100 text-sm font-bold mb-2" htmlFor="symbol">
               Symbol
             </label>
             <Field
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 dark:text-neutral-200 leading-tight focus:outline-none focus:shadow-outline"
               id="symbol"
               type="text"
               placeholder="symbol"
